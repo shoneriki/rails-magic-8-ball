2f4bcf81dd8fe81a5bba4141c7f107b06110d6b2
shoneriki <ikirenohs@gmail.com>
2022-02-09 11:32:38 +0900

    elementary reply page gets a response

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,14 @@
 class QuestionsController < ApplicationController
+  POSSIBLE_RESPONSES = ['It is certain', 'It is decidedly so.', 'Yes definitely.', 'Without a doubt.', ' You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Yes.', 'Signs point to yes.','Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',]
+
   def ask
   end
 
   def reply
-    
+    if params[:question] != ""
+      @reply = POSSIBLE_RESPONSES.sample
+    else
+      @reply = "ask something"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'questions#ask'
   get "/ask", to: "questions#ask"
-  post "/reply", to: "questions#reply"
+  get "/reply", to: "questions#reply"
 end
