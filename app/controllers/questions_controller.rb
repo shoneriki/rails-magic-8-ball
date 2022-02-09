1b4c38af252d74a11d7faefb3540ef6903c65ad1
shoneriki <ikirenohs@gmail.com>
2022-02-09 12:43:09 +0900

    completed possible responses array

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  POSSIBLE_RESPONSES = ['It is certain', 'It is decidedly so.', 'Yes definitely.', 'Without a doubt.', ' You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Yes.', 'Signs point to yes.','Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.']
+  POSSIBLE_RESPONSES = ['It is certain', 'It is decidedly so.', 'Yes definitely.', 'Without a doubt.', ' You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Yes.', 'Signs point to yes.','Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', "Don't count on it.", 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 
   ASK_BEGINNINGS = ['are', 'will', 'is', 'should', 'am', 'did', 'does']
 
