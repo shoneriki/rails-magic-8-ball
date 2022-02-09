5f8e0d4ffe8f6ddc4c116f36f55facf978785e31
shoneriki <ikirenohs@gmail.com>
2022-02-09 11:49:50 +0900

    reply to accept yes/no questions only

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,16 @@
 class QuestionsController < ApplicationController
-  POSSIBLE_RESPONSES = ['It is certain', 'It is decidedly so.', 'Yes definitely.', 'Without a doubt.', ' You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Yes.', 'Signs point to yes.','Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.',]
+  POSSIBLE_RESPONSES = ['It is certain', 'It is decidedly so.', 'Yes definitely.', 'Without a doubt.', ' You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Yes.', 'Signs point to yes.','Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.']
+
+  ASK_BEGINNINGS = ['are', 'will', 'is', 'should', 'am', 'did', 'does']
 
   def ask
   end
 
   def reply
-    if params[:question] != ""
+    if params[:question] != "" &&  params[:question].downcase.starts_with?(*ASK_BEGINNINGS)
       @reply = POSSIBLE_RESPONSES.sample
     else
-      @reply = "ask something"
+      @reply = "Please ask a yes or no question that deals with your future decisions"
     end
   end
 end
